beb01865ab03c33cdbd437e35498aa7b475544b1
udithanayanajith <uditha.nayanagith@gmail.com>
2022-02-22 04:15:37 +0530

    Local data set added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
+import ReactDom from 'react-dom';
+import CityDataLocal from "./data/cities.json";
+import { Button } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+  const [cityid, setCityid] = useState('')
+
+  const loadedData = JSON.stringify(CityDataLocal.List);
+  const city = JSON.parse(loadedData);
+  console.log("App on");
+  console.log(city);
+  console.log(cityid,"Paasing througth down");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&appid=7bb92bcfbe80e3de3eed227c8ef14cb6`
+    const getId = (event) => {
+
+        axios.get(url).then((response) => {
+          setData(response.data)
+          console.log(response.data,"data")
+        })
+      
+    }
+
+  return ( 
+    <div className="app">
+      <div className="search">
+
+      {city.map((city,id)=>
+      <span>
+        <Button variant="contained"   onClick={event =>getId()+ setCityid(city.CityCode)+console.log(city.CityCode)}>{city.CityName}</Button>
+      </span>
+      
+      )}
+
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
+
+
+        <sapn center>Uditha Nayanajith@2022 </sapn>
+      </div>
     </div>
   );
 }
-
-export default App;
